e2699e169259d90fe539f8a5ec762d647a0eca5f
Juan Castorino <juan.octopia@gmail.com>
2021-08-12 19:35:32 -0300

    test h2

--- tests/unit/components/counter.spec.js
+++ tests/unit/components/counter.spec.js
@@ -6,12 +6,22 @@ import Counter from '@/components/Counter'
 
 describe('Counter Component', () => {
 
-    test('debe de hacer match con el snapshot', () => {
+    // test('debe de hacer match con el snapshot', () => {
 
+    //     const wrapper = shallowMount(Counter)
+
+    //     expect(wrapper.html()).toMatchSnapshot()
+
+    // })
+
+    test(('H2 debe tener el valor por defecto COUNTER'), () => {
         const wrapper = shallowMount(Counter)
 
-        expect(wrapper.html()).toMatchSnapshot()
+        expect(wrapper.find('h22').exists()).toBe(false)
 
+        const h2Value = wrapper.find('h2').text()
+
+        expect(h2Value).toBe('Counter')
     })
 
 })
